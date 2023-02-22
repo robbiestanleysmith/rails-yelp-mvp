8a98aabf27945166c72f99bd9f10af465e41a4d6
Robbie Stanley-Smith <robbie.stanleysmith@gmail.com>
2023-02-22 16:31:53 +0000

    models all raking green

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :phone_number, :category, presence: true
+  validates :category, acceptance: { accept: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
